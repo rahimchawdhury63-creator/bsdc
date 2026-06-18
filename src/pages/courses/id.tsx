903af62ae5fb2c29b9ed875607cd94fcb8907fb1
rahimchawdhury63-creator <rahimchawdhury63@gmail.com>
2026-06-18 22:34:10 +0600

    Add files via upload

---
+++ src/pages/courses/id.tsx
@@ -0,0 +1,3 @@
+import { SEOHead } from '@components/seo/SEOHead';
+import { CoursePage } from '@components/courses/CoursePage';
+export const CourseDetailPage = () => <><SEOHead title="Course" canonicalPath="/courses" /><CoursePage /></>;
---
+++ src/pages/courses/index.tsx
@@ -0,0 +1,3 @@
+import { SEOHead } from '@components/seo/SEOHead';
+import { CourseList } from '@components/courses/CourseList';
+export const CoursesPage = () => <><SEOHead title="Courses" canonicalPath="/courses" /><CourseList /></>;
