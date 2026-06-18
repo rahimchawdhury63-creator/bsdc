ddd456296ac9172f8bc890932db885dc83da4f89
rahimchawdhury63-creator <rahimchawdhury63@gmail.com>
2026-06-18 22:40:06 +0600

    Add files via upload

---
+++ src/pages/creator/ads.tsx
@@ -0,0 +1,3 @@
+import { AdCreator } from '@components/ads/AdCreator';
+import { AdManager } from '@components/ads/AdManager';
+export const CreatorAdsPage = () => <div className="wallet-grid"><AdCreator /><AdManager /></div>;
---
+++ src/pages/creator/dashboard.tsx
@@ -0,0 +1,5 @@
+import { CreatorDashboard } from '@components/creator/CreatorDashboard';
+import { EarningsChart } from '@components/creator/EarningsChart';
+import { MonetizationApply } from '@components/creator/MonetizationApply';
+import { RankingSystem } from '@components/creator/RankingSystem';
+export const CreatorDashboardPage = () => <div className="wallet-grid"><CreatorDashboard /><EarningsChart /><MonetizationApply /><RankingSystem /></div>;
