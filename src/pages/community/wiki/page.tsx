8089777b102d9bb3f74d542b710fa86403d623c2
rahimchawdhury63-creator <rahimchawdhury63@gmail.com>
2026-06-18 22:32:23 +0600

    Add files via upload

---
+++ src/pages/community/wiki/page.tsx
@@ -0,0 +1,4 @@
+import { WikiPage } from '@components/community/WikiPage';
+import { SEOHead } from '@components/seo/SEOHead';
+/** Wiki route shell for community wiki pages. */
+export const WikiRoutePage = () => <><SEOHead title="BSDC Wiki" canonicalPath="/wiki" /><WikiPage /></>;
