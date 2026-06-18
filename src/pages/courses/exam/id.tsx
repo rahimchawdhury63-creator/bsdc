4fa27d70998cf1a1ff73682e9f7490240d519ed9
rahimchawdhury63-creator <rahimchawdhury63@gmail.com>
2026-06-18 22:38:02 +0600

    Add files via upload

---
+++ src/pages/courses/exam/id.tsx
@@ -0,0 +1,3 @@
+import { SEOHead } from '@components/seo/SEOHead';
+import { ExamSystem } from '@components/courses/ExamSystem';
+export const CourseExamPage = () => <><SEOHead title="Course exam" canonicalPath="/courses/exam" noIndex /><ExamSystem /></>;
